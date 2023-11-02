6db4d2f085d5503e218b329d1ff6495a15ba3cbe
Alphonce Kochieng <dulkochieng@gmail.com>
2023-11-02 10:19:46 +0300

    update the binary tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,6 +1,10 @@
 #include "binary_trees.h"
-
-
+/**
+  *binary_tree_delete - deletes binary trees
+  *@tree: the tree
+  *
+  *Return: void
+  */
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
